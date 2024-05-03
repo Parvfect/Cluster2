3d992a079419e83023351688eca3369acdd6a538
Parvfect <parvagrw02@gmail.com>
2024-05-03 11:53:32 +0100

    Changed to 412

--- dcc.py
+++ dcc.py
@@ -321,7 +321,7 @@ def decoding_errors_fer(k, n, dv, dc, ffdim, P, H, G, GF, graph, C, symbols, n_m
         with open(write_path, "a") as f:
             f.write(f"\nIterations {iterations} Failures {decoding_failures}")
         
-    final_write_path = os.path.join(os.environ['HOME'], "results_13.txt")
+    final_write_path = os.path.join(os.environ['HOME'], "results_12_412.txt")
     with open(final_write_path, "a") as f:
         f.write(f"\nIterations {iterations} Failures {decoding_failures}")
          
@@ -344,10 +344,10 @@ def run_fer(n_motifs, n_picks, dv, dc, k, n, L, M, ffdim, P, code_class="", iter
     
 if __name__ == "__main__":
     n_motifs, n_picks = 8, 4
-    dv, dc, ffdim, P = 3, 9, 67, 2 * 0.038860387943791645 
+    dv, dc, ffdim, P = 4, 12, 67, 2 * 0.038860387943791645 
     k, n = 30, 45
     L, M = 50, 1002
-    read_lengths = np.arange(13,14)
+    read_lengths = np.arange(12,13)
         
     Harr = []
     masked=False
