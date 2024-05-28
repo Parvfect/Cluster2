719a2fde525f6d85e449a942bc8d8f28eeccfe1b
Parvfect <parvagrw02@gmail.com>
2024-05-28 12:55:29 +0100

    Read length of 7

--- cc.py
+++ cc.py
@@ -287,7 +287,7 @@ if __name__ == "__main__":
     k, n = 30, 45
     L, M = 50, 1002
     read_length = 6
-    read_lengths = np.arange(5, 6)
+    read_lengths = np.arange(7, 8)
 
     masked = True
 
