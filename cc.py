6138b43f3898f8bd8e0d5cce75f0be2b7c1fa4f0
Parvfect <parvagrw02@gmail.com>
2024-05-29 09:10:12 +0100

    RL 9 CC

--- cc.py
+++ cc.py
@@ -287,7 +287,7 @@ if __name__ == "__main__":
     k, n = 30, 45
     L, M = 50, 1002
     read_length = 6
-    read_lengths = np.arange(7, 8)
+    read_lengths = np.arange(8, 9)
 
     masked = True
 
